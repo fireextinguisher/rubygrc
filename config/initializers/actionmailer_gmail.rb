223776add4ce5f7fb89114242cf1c3db33171762
Denis <ground@mail.ru>
2012-09-19 01:21:38 +0400

    actionmailer_gmail.rb update

--- config/initializers/actionmailer_gmail.rb
+++ config/initializers/actionmailer_gmail.rb
@@ -1,12 +1,14 @@
-ActionMailer::Base.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :address  => "smtp.gmail.com",
-  :port  => 587,
-  :user_name  => "rubygrc2012@gmail.com",
-  :password  => "orionrubygrc2012",
-  :enable_starttls_auto => true,
-  :authentication  => :plain
-}
-
+require 'tlsmail'
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :tls => true,
+  :domain => 'mail.google.com',  # mail.customdomain.com if you use google apps
+  :authentication => :plain,
+  :user_name  => 'rubygrc2012@gmail.com',  # make sure you include the full email address
+  :password  => 'orionrubygrc2012'
+}
