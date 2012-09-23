367f919e4389b833e8dc804bdf335b327a0a779d
Denis <ground@mail.ru>
2012-09-23 22:28:25 +0400

    assets precompile Heroku error

--- config/application.rb
+++ config/application.rb
@@ -79,6 +79,9 @@ module RubyGRC
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Remove "could not connect to server" error on Heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'	
