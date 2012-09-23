66079675e57edda771aa6c1152975cca95c763e5
Denis <ground@mail.ru>
2012-09-24 01:27:53 +0400

    bundler for compass_twitter_bootstrap

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,9 @@ Net.instance_eval {remove_const :SMTPSession} if defined?(APOPSession)
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  #Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module RubyGRC
